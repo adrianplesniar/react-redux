8102df1242daa91b6aed0eff277d9642aba8942f
DexterWymiatacz <dexterwymiatacz@gmail.com>
2021-06-10 23:45:31 +0200

    #14 - Outputting lists

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,8 @@ function App() {
     <div className="App">
       <h1>My first React App</h1>
       <p>Welcome :)</p>
-      <Ninjas />
+      <Ninjas name="Ryu" age="25" belt="black"/>
+      <Ninjas name="Twoja Stara" age="69" belt="white"/>
     </div>
   );
 }
