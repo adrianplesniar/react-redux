7a767bbd7c4008c9ddd8bc3e09db74910b485160
DexterWymiatacz <dexterwymiatacz@gmail.com>
2021-06-13 23:29:39 +0200

    #15 - Stateless Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
 import Ninjas from "./Ninjas";
 
 function App() {
+    const ninjas = [
+      { name: "Ryu", age: 30, belt: "black", id: 1 },
+      { name: "Yoshi", age: 20, belt: "green", id: 2 },
+      { name: "Benedict Cumberbatch", age: 43, belt: "Old black", id: 3 }
+    ]
   return (
     <div className="App">
       <h1>My first React App</h1>
       <p>Welcome :)</p>
-      <Ninjas name="Ryu" age="25" belt="black"/>
-      <Ninjas name="Twoja Stara" age="69" belt="white"/>
+      <Ninjas ninjas={ninjas}/>
     </div>
   );
 }
