d64db85327d6026c5d377cd3d5870f3afb306f2e
DexterWymiatacz <dexterwymiatacz@gmail.com>
2021-07-05 23:59:57 +0200

    17 v2

---
+++ my-app/src/AddNinja.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class AddNinja extends Component {
+    render(){
+        return (
+            <div>
+                <form>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" onChange={} />
+                </form>
+            </div>
+        )
+    }
+}
