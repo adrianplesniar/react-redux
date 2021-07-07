37614e1d6871aa40ee0a45f8022869fa0bab8df7
DexterWymiatacz <dexterwymiatacz@gmail.com>
2021-07-08 00:37:11 +0200

    #18 - Functions as Props

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Ninjas = ({ninjas}) => {
     // const ninjaList = ninjas.map(ninja => {
-        // if(ninja.age > 20){
-        //     return (
-        //         <div className="ninja" key={ninja.id}>
-        //             <div>Name: { ninja.name }</div>
-        //             <div>Age: { ninja.age }</div>
-        //             <div>Belt: { ninja.belt }</div>
-        //         </div>
-        //     )
-        // } else{
-        //     return null
-        // }
+    //     if(ninja.age > 20){
+    //         return (
+    //             <div className="ninja" key={ninja.id}>
+    //                 <div>Name: { ninja.name }</div>
+    //                 <div>Age: { ninja.age }</div>
+    //                 <div>Belt: { ninja.belt }</div>
+    //             </div>
+    //         )
+    //     } else{
+    //         return null
+    //     }
     return(
         <div className="ninja-list">
             {
                 ninjas.map(ninja => {
-                    ninja.age > 20 ? (
+                    return ninja.age > 20 ? (
                         <div className="ninja" key={ninja.id}>
                             <div>Name: { ninja.name }</div>
                             <div>Age: { ninja.age }</div>
